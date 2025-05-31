ad91d568be743a8f2edd1089113daf9cf7e999ea
Estebandev-ved <esteban.rubiano@correo.tdea.edu.co>
2025-05-31 10:31:27 -0500

    Delete atm/atm directory

--- atm/atm/src/main/java/com/bancobit/atm/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bancobit.atm.repository;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bancobit.atm.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    Optional<Cliente> findByIdentificacion(String identificacion);
-        
-}
